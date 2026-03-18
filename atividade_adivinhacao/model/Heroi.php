aa1f7021020004cdada897cada718c5b45816924
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-18 09:57:50 -0300

    Create Heroi.php

---
+++ atividade_adivinhacao/model/Heroi.php
@@ -0,0 +1,77 @@
+<?php
+
+class Heroi{
+    private string $nome;
+    private string $link;
+    private string $dica;
+
+    public function __construct(string $nome, string $link, string $dica){
+        $this->nome = $nome;
+        $this->link = $link;
+        $this->dica = $dica;
+    }
+
+    public function __toString(){
+        $dados = "Nome: " . $this->nome . "|";
+        $dados .= "Link: " . $this->link . "|";
+        $dados .= "Dica: " . $this->dica . "|";
+        return $dados;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of link
+     */
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set the value of link
+     */
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dica
+     */
+    public function getDica(): string
+    {
+        return $this->dica;
+    }
+
+    /**
+     * Set the value of dica
+     */
+    public function setDica(string $dica): self
+    {
+        $this->dica = $dica;
+
+        return $this;
+    }
+}
+
+
+?>
