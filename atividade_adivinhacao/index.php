ac45c61e96f019d9ac31242b6353239d2638023f
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-18 09:58:37 -0300

    Create index.php

---
+++ atividade_adivinhacao/index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo de Adivinhação</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <h1>Bem-Vindo ao jogo de Adivinhação</h1>
+</body>
+</html>
