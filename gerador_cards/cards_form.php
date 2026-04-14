a3b55462fee1cb3238021a8559746e3586e40143
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-04-13 22:04:31 -0300

    Update cards_form.php

--- gerador_cards/cards_form.php
+++ gerador_cards/cards_form.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="form.css">
     <title>Cards</title>
 </head>
 <body>
@@ -50,7 +50,7 @@
 
         <div class="">
             <label for="">Link: </label>
-            <input type="link" name="link">
+            <input type="text" name="link">
             <br>
         </div>
 
