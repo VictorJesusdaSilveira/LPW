a4351d5bbe6774ccb722f475110b78e7c04fe3bf
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-04-29 09:47:07 -0300

    Update livros.php

--- biblioteca/livros.php
+++ biblioteca/livros.php
@@ -1,93 +1,125 @@
 <?php
+
+//Mostrar erros
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once("util/Conexao.php");
 
 $conexao = Conexao::getConexao();
+//print_r($conexao);
 
-//listagem dos livros
-$sql = "SELECT * FROM livros";
-$stm = $conexao->prepare($sql); //prepara
-$stm->execute(); //executa
-$livros = $stm->fetchAll(); //pega a resposta
+//Salvar o livro
+if(isset($_POST['titulo'])) {
+    //Receber os dados do formulário 
+    $titulo = $_POST["titulo"];
+    $genero = $_POST["genero"];
+    $paginas = $_POST["paginas"];
+    $autor = $_POST["autor"];
 
-//pegar o livro
-if(isset($_POST["titulo"])){
+    //Inserir no banco
+    $sql = "INSERT INTO livros (titulo, genero, qtd_paginas, autor) VALUES(?, ?, ?, ?)";
+    $stm = $conexao->prepare($sql);
+    $stm->execute(array($titulo, $genero, $paginas, $autor));
 
+    //Redirecionar para a página de listagem
+    header("location:livros.php");
+
+    
 }
 
-?>
+//Listagem dos livros
+$sql = "SELECT * FROM livros";
+$stm = $conexao->prepare($sql);
+$stm->execute();
+$livros = $stm->fetchAll();
 
+//print "<pre>" . print_r($livros, true) . "</pre>";
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Livro</title>
+    <title>Cadastro de livros</title>
 </head>
 <body>
-    
+
     <h1>Cadastro de livros</h1>
 
     <h3>Listagem</h3>
-    
+
     <table border="1">
+        <!--Cabeçalho-->
         <tr>
             <th>ID</th>
+            <th>Autor</th>
             <th>Título</th>
             <th>Gênero</th>
             <th>Páginas</th>
-        </tr>
+            <th></th>
+        </tr> 
 
-        <?php foreach ($livros as $l) :?>
+        <!--Dados-->
+        <?php foreach($livros as $l): ?>
             <tr>
                 <td><?= $l["id"] ?></td>
+                <td><?= $l["autor"] ?></td>
                 <td><?= $l["titulo"] ?></td>
-                <td><?php if($l["genero"] == "D"){
-                    print "Drama";
-                }else if ($l["genero" == "F"]){
-                    print "Fantasia";
-                }else if ($l["genero"] == "R"){
-                    print "Romance";
-                }else{
-                    print "Outro";
-                }
-                    
-                    ?></td>
-                <td><?= $l["qtd_paginas"]?></td>
+                <td>
+                    <?php
+                        if($l['genero'] == 'D')
+                            print "Drama";
+                        else if($l['genero'] == 'F')
+                            print "Ficção";
+                        else if($l['genero'] == 'R')
+                            print "Romance";
+                        else if($l['genero'] == 'O')
+                            print "Outro";                        
+                    ?>
+                </td>
+                <td><?= $l["qtd_paginas"] ?></td>
+                <td><a href="livros_excluir.php?id=<?= $l["id"] ?>" onclick="if(! confirm('Confirme a exclusão do livro')) return false;">Excluir</a></td>
             </tr>
-
-        <?php endforeach; ?>    
+        
+        <?php endforeach; ?>
     </table>
 
 
-    <h3>Formulários</h3>
+    <h3>Formulário</h3>
 
     <form action="" method="POST">
-        <label for="titulo">Título:</label>
-        <input type="text" placeholder="Informe o título: " name="titulo">
 
-        <br>
+        <label for="">Autor</label>
+        <input type="text" placeholder="Informe o Autor" name="autor">
+        <br><br>
 
-        <p>Gêneros:</p>
-        <select name="genero" id="">
-            <option value="">---- Selecione o Gênero ----</option>
+        <label for="">Título</label>
+        <input type="text" placeholder="Informe o título"
+            name="titulo">
+
+        <br><br>
+
+        <select name="genero">
+            <option value="">---Selecione o gênero---</option>
             <option value="D">Drama</option>
-            <option value="R">Romance</option>
             <option value="F">Ficção</option>
+            <option value="R">Romance</option>
             <option value="O">Outro</option>
         </select>
 
-        <br>
+        <br><br>
 
-        <label for="qtd_paginas">Quantidade de Páginas:</label>
-        <input type="number" placeholder="Informe a quantidade de páginas" name="qtd_paginas">
+        <label for="">Quantidade de Páginas</label>
+        <input type="number" name="paginas" 
+            placeholder="Informe o número de páginas">
 
-            <br>
+        <br><br>
 
-        <button type="submit">Enviar</button>
-
+        <button>Gravar</button>
 
     </form>
-
-
+    
 </body>
 </html>
