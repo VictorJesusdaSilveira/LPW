7437493d8a714a687fb412a551fcc59c27c1ee38
Victor JS <victorj.ifpr2024@gmail.com>
2026-06-17 09:57:19 -0300

    Add files via upload

---
+++ crud_alunos/crud_alunos/index.php
@@ -0,0 +1,5 @@
+<?php
+
+//Redireciona para a listagem de alunos
+header("location: ./view/alunos/listar.php");
+    
---
+++ crud_alunos/crud_alunos/view/alunos/listar.php
@@ -0,0 +1,22 @@
+<?php
+require_once(__DIR__ . "/../../util/Connection.php");
+
+//Teste da Conexão
+$conn = Connection::getConnection();
+//print_r($conn);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de alunos</title>
+</head>
+<body>
+    
+</body>
+</html>
